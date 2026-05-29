da847a92c1d00ff698fac79572e893a96a444958
Pq989-j <marco2006aureliorm@gmail.com>
2026-05-29 19:08:25 +0200

    feat: servidor Express con rutas GET básicas

--- index.js
+++ index.js
@@ -24,12 +24,28 @@ const array = [
         { id: 5, nombre: "2001: A space Odyssey"}
     ];
 
-app.get("/api/entidad", (req, res) => {
-    return res.status(200).json(array)
+app.get("/api/peliculas", (req, res) => {
+    return res.status(200).json(array);
 });
 
-app.get("/api/entidad/:id", (req, res) => {
-  const nombre = Number(req.params.id);
+
+app.get("/api/peliculas/:id", (req, res) => {
+    const id = Number(req.params.id);
+  const peli = array.find((p) => p.id === id);
+    if (!peli) {
+        return res.status(404).json({ error: "Pelicula no encontrada" });
+    }
+
+  return res.json(peli);
+});
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}]${req.method}${req.url}`);
+    next();
+});
+
+app.get("/api/saludo/:nombre", (req, res) => {
+  const nombre = req.params.nombre;
 
   if (nombre.length < 2) {
     return res.status(400).json({ error: "El nombre es muy corto" });
@@ -38,27 +54,21 @@ app.get("/api/entidad/:id", (req, res) => {
   return res.status(200).json({ mensaje: `Hola, ${nombre}` });
 });
 
-app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}]${req.method}${req.url}`);
-    next();
-});
-
-
-app.post("/api/entidad", (req,res)=> {
+app.post("/api/peliculas", (req,res)=> {
     console.log(req.body)
-    const{id,nombre}=req.body
+    const{nombre}=req.body
 
-    if(!id || !nombre) {
+    if(!nombre) {
         return res.status(400).json({error: "Faltan titulo o contenido"})
     }
 
     const nuevapeli = {
         id: array.length + 1,
-        nombre,
+        nombre
     };
 
     array.push(nuevapeli);
-    return res.status(200).json(nuevapeli);
+    return res.status(201).json(nuevapeli);
 });
 
 
